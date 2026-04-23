135faeeeee7da797fe081c6311c9df2dd9ee996d
Jl Sut Pi <sutpi070@gmail.com>
2026-04-23 17:55:18 +0700

    fix(ColMovieCard): add error handling and fix Tailwind classes

--- src/pages/discover/Company.jsx
+++ src/pages/discover/Company.jsx
@@ -32,6 +32,8 @@ const Company = () => {
     fetchMovie();
   }, [company_id]);
 
+  console.log(movies, series);
+
   if (loading)
     return (
       <div className=" text-white pt-20 xl:px-[75px] lg:px-[45px]   md:px-[30px] px-5">
