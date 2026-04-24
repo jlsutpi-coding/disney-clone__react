57029b100579eb94e4c96326d55cb8445d7aabed
Jl Sut Pi <sutpi070@gmail.com>
2026-04-24 16:20:04 +0700

    refactor: replace Embla Carousel with Swiper
    - Remove embla-carousel-react dependency
    - Add swiper and swiper/react packages
    - Update Slider component to use Swiper with pagination
    - Fix dot indicators not showing (add core CSS imports)
    - Remove unused carousel options and logic

--- src/pages/home/Slider.jsx
+++ src/pages/home/Slider.jsx
@@ -1,12 +1,22 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import GlobalApi from "../../services/GlobalApi.jsx";
-import EmblaCarousel from "./EmblaCarousel.jsx";
 import DetailSkeleton from "../../components/DetailSkeleton.jsx";
+import CarouselItem from "./CarouselItem.jsx";
+import { GenresContext } from "../../context/GenresContext.jsx";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Pagination } from "swiper/modules";
+
+import "swiper/css/pagination";
 
 const Slider = () => {
   const [movieList, setMovieList] = useState([]);
+  const { genres } = useContext(GenresContext);
 
+  console.log(genres);
   const OPTIONS = { loop: true };
 
   useEffect(() => {
@@ -16,9 +26,21 @@ const Slider = () => {
     };
     fetchData();
   }, []);
+  console.log(movieList);
 
   if (movieList.length === 0) return <DetailSkeleton />;
-  return <EmblaCarousel slides={movieList} options={OPTIONS} />;
+  return (
+    <Swiper pagination={{ clickable: true }} modules={[Pagination]}>
+      {movieList.map((movie) => (
+        <SwiperSlide key={movie.id}>
+          <CarouselItem
+            movie={movie}
+            genres={movie.media_type === "movie" ? genres?.movie : genres?.tv}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default Slider;
