d4870abef184ab7c589286e3726d28832aad8d50
Jl Sut Pi <sutpi070@gmail.com>
2026-05-01 12:49:27 +0700

    feat:  persist likes to localstorage

--- src/redux/slices/likeSlice.js
+++ src/redux/slices/likeSlice.js
@@ -1,8 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  likedItems: [],
+const loadInitialState = () => {
+  try {
+    const data = localStorage.getItem("movie_likes");
+    if (!data) {
+      return {
+        likedItems: [],
+      };
+    }
+    return { likedItems: JSON.parse(data) };
+  } catch (error) {
+    console.error(error);
+  }
+  return { likedItems: [] };
 };
+
+const initialState = loadInitialState();
 
 const likeSlice = createSlice({
   name: "likes",
@@ -14,11 +27,13 @@ const likeSlice = createSlice({
       );
       if (existingItem) return;
       state.likedItems.push(payload);
+      uploadToLocalStorage(state.likedItems);
     },
     removeFromLikes: (state, { payload }) => {
       const existingItem = state.likedItems.some((item) => item.id === payload);
       if (!existingItem) return;
       state.likedItems = state.likedItems.filter((item) => item.id !== payload);
+      uploadToLocalStorage(state.likedItems);
     },
   },
 });
@@ -26,6 +41,10 @@ const likeSlice = createSlice({
 export const selectLikedItems = (state) => state.likes.likedItems;
 export const selectIsInLikedItems = (state, movieId) => {
   return state.likes.likedItems.some((item) => item.id === movieId);
+};
+
+const uploadToLocalStorage = (likedItems) => {
+  localStorage.setItem("movie_likes", JSON.stringify(likedItems));
 };
 
 export const { addToLikes, removeFromLikes } = likeSlice.actions;
